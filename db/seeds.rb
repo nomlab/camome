23c6dc153754150104d92b98025ddfa2f62423f3
Yuya Murata <murata1224@gmail.com>
2015-03-20 13:23:52 +0900

    Create seed data for admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def create_admin_user
+  admin_user = User.new(:name => "admin_user")
+  admin_user.master_pass = "admin"
+  admin_auth_info = MasterAuthInfo.new(:login_name => "admin")
+  admin_auth_info = KeyVault.lock(admin_auth_info, admin_user)
+  admin_user.auth_info = admin_auth_info
+  admin_user.save
+  puts "ok: admin user is created."
+end
+
+create_admin_user
