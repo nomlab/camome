77a284c51d104c7f29b14bfa18a748dfd0c990c8
yuko <ike534xx@yahoo.co.jp>
2015-04-28 19:24:45 +0900

    Create inbox for recurrences

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -3,4 +3,9 @@ class InboxController < ApplicationController
   def missions
     @events = Event.all
   end
+
+  def recurrences
+    @events = Event.all
+    @recurrences = Recurrence.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,10 @@ Rails.application.routes.draw do
   get "gate/logout"
   post "gate/login"
 
-  get "inbox/missions"
   match "events/create_recurrence", :via => :post
-  resources :events
+  get "inbox/missions"
+  get "inbox/recurrences"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
