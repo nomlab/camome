254faebf25cb470ca706e3bc94a28fbe686789e2
yuko <ike534xx@yahoo.co.jp>
2015-05-07 11:40:19 +0900

    Disable turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require turbolinks
 //= require moment
 //= require fullcalendar
 //= require bootstrap-datetimepicker
